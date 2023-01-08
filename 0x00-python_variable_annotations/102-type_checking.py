15884bff382a3f189236f0a670894dd76fbd0339
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-01-08 16:32:47 +0100

    wawoo

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,18 +4,18 @@
 from typing import Any, Sequence, Union, Tuple, List, TypeVar
 
 
-def zoom_array(lst: List[Any], factor: Union[int, float] = 2) -> List[Any]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     '''Annotate a zoom_array function prperly
     '''
-    zoomed_in: List[Any] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
