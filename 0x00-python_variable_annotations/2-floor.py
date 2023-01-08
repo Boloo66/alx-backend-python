1ed6fbd3fa710d1f9928eade5f759617d4aac1be
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-01-08 11:52:22 +0100

    floor of float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' Returnn floor value of any float
+'''
+
+
+from math import floor
+
+
+def floor(n: float) -> int:
+    '''Returns floor value of n float type
+    '''
+    return(floor(n))
