cff24d48e87748306e8d124f87b8c3ad06dd2fc2
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-01-11 00:27:06 +0100

    I did it

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Asyncio module for multiple coroutines
+'''
+import random
+import asyncio
+from typing import List, Union, Any
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    ''' spawn wait_random coroutine from with
+    '''
+    my_list: List[Union[float, Any]] = []
+    for _ in range(n):
+        task1 = asyncio.create_task(wait_random(max_delay))
+        value = await task1
+        my_list.append(value)
+    return my_list
