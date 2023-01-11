df8cdaf715e94bdbd821c2ec0c110efe5930801d
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-01-11 02:46:16 +0100

    adjustments made

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,12 +7,9 @@ from typing import List, Union, Any
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     ''' spawn wait_random coroutine from with
     '''
-    my_list: List[Union[float, Any]] = []
-    for _ in range(n):
-        task1 = asyncio.create_task(task_wait_random(max_delay))
-        value = await task1
-        my_list.append(value)
-    return sorted(my_list)
+    wait_time = await asyncio.gather(
+        *list(map(lambda _: task_wait_random(max_delay), range(n))))
+    return sorted(wait_time)
